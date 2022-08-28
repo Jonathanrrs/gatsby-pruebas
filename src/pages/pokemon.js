141b1814756fd328b13745a62f127ed9979282a0
jonathan <yoni_12rrs@hotmail.com>
2022-08-28 15:28:44 -0500

    initial config

---
+++ src/pages/pokemon.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+const Pokemon = ({ serverData }) => {
+  return (
+    <div>
+      <Helmet>
+      <title>Pokemon</title>
+      <meta name="description" content={serverData.name} />
+      <meta property="og:image" content="https://myfirstgatsbysitemaster63551.gatsbyjs.io/Background.png" />
+      </Helmet>
+      <h1>Welcome</h1>
+      <h1>Nueva página Pokemon</h1>
+      <h1>{serverData.name}</h1>
+      <p>ID: {serverData.id}</p>
+      <b>Peso: ${serverData.height}</b>
+      {/* <img src="./Background.png" alt="Background" /> */}
+    </div>
+  );
+};
+
+export default Pokemon;
+
+export async function getServerData() {
+  try {
+    const data = await fetch("https://pokeapi.co/api/v2/pokemon/raichu");
+    const dataToJson = await data.json();
+    return {
+      props: dataToJson
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}

412628ae85bdc210a82e3e3dbc25082bcc630af6
Jonathanrrs <yoni_12rrs@hotmail.com>
2023-03-28 10:46:07 -0600

    test post processing

---
+++ src/pages/loading.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <span class="loader"></span>
+    </div>
+  );
+};
+
+export default Loading;
